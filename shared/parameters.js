a3a56a0a39b039eda8373f98ddac69ac06857751
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2022-10-06 14:53:29 +0200

    delete offline, online and shared folders

--- shared/parameters.js
+++
@@ -1,9 +0,0 @@
-const language = hu; //available languages: english (en), hungarian (hu), french (fr), portoguise (pt)
-const numberOfPracticeBlocks = 2;
-const numberOfBlocks = 20;
-const numberOfBlockElements = 85;
-const numberOfSequenceRepetitions = 10;
-const patternTrialImage = "url(../static/images/dalmata.jpg)";
-const randomTrialImage = "url(../static/images/dalmata.jpg)";
-const rsi = 120;
-const initialDelay = 1000;

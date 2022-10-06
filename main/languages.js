32f13728a46ec6b0545be5d65e125ceb0a29dc2a
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2022-10-06 14:55:09 +0200

    add warning stimuli texts

---
+++ main/languages.js
@@ -0,0 +1,344 @@
+const en = {
+    welcomePage: {
+        welcome: "Welcome!",
+        clickNext: "Click on <strong>Next</strong> to continue."
+    },
+    instruction: {
+        fourCircles: "In this task, you will see four circles on the screen. From left to right, the <strong>'S'</strong>, <strong>'F'</strong>, <strong>'J'</strong> and <strong>'L'</strong> keys correspond to the four circles.",
+        dog: "From time to time, an <b>image of a dog</b> will appear in one of the circles.",
+        yourTask: "Your task will be to 'catch the dog' by pressing the key corresponding to the position of the dog <strong>as quickly and as accurately as you can</strong>.",
+        restBetweenBlocks: "In every few minutes, there will be a short pause, when we will show you your average accuracy and response time, which you can use to ameliorate your performance.",
+        img: "<img src='../static/images/ASRT_en.gif' width='30%' alt='Dalmata'/>"
+    },
+    instruction2: {
+        firstButton: "Your will need to press the <strong>'S'</strong> key with the middle finger of your left hand,",
+        secondButton: "the <strong>'F'</strong> key with the index finger of your left hand</strong>",
+        thirdButton: "the <strong>'J'</strong> key with the index finger of your right hand,",
+        fourthButton: "the <strong>'L'</strong> key with the middle finger of your right hand</strong>.",
+        ifClear: "If the instructions are clear, click on <strong>Next</strong>; if not, you can go back and check the instructions again by clinking on <strong>Previous</strong>."
+    },
+    practice: {
+        startPractice: "If you are ready, press <strong>any</strong> key to start a practice!",
+        practiceSoon: "Now a short practice will start."
+    },
+    task: {
+        realTask: "The real task begins now.",
+        startTask: "If you are ready, press <strong>any</strong> key to start the task!",
+        nextBlock: "Press any key to start the next block!",
+        nextBlockSoon: "The next block starts soon.",
+        place: "Please place your fingers in this position on the keyboard:"
+    },
+    end: {
+        endTask: "End of the experiment.",
+        thankYou: "Thank you!"
+    },
+    feedback: {
+        endBlock: "End of block ",
+        yourAccuracy: "Your accuracy: ",
+        yourRt: "Your average response time: ",
+        moreAccurate: "Try to be more accurate!",
+        faster: "Try to be faster!",
+        continue: "Nice job!"
+    },
+    button: {
+        next: "Next",
+        previous: "Previous"
+    },
+    fullscreen: {
+        fullscreenText: "The experiment will switch to full screen mode when you press the button below.",
+        fullscreenButton: "Continue"
+    },
+    parameters: {
+        subject: "Subject Number:",
+        session: "Session Number:"
+    },
+    startWarning: {
+        startSubject: "You are about to start the task with<strong> Subject Number ",
+        startSession: "</strong> and <strong>Session Number ",
+        startButton: "Start the task!",
+        goBackButton: "Change the parameters"
+    },
+    warning: {
+        warning: "WARNING!",
+        accuracy: "Your accuracy throughout the whole task:",
+        onlyValid: "We can only accept your submission if your accuracy is above 80% by the end of the task!",
+        moreAccurate: "Please try to be more accurate!",
+        pressButton: "Please press a button to continue!"
+    }
+}
+
+const hu = {
+    welcomePage: {
+        welcome: "Üdvözlünk!",
+        clickNext: "Kattints a <strong>Tovább</strong> gombra a folytatáshoz!"
+    },
+    instruction: {
+        fourCircles: "Ebben a feladatban négy kört fogsz látni a képernyő közepén. Balról jobbra az <strong>'S'</strong>, <strong>'F'</strong>, <strong>'J'</strong> és <strong>'L'</strong> billentyűk tartoznak a négy körhöz.",
+        dog: "Időről időre egy <b>kutyafej</b> fog megjelenni valamelyik körben.",
+        yourTask: "A feladatod az lesz, hogy 'elkapd a kutyát' a kutyafej megjelenési helyéhez tartozó gomb <strong>minél pontosabb és minél gyorsabb lenyomásával</strong>.",
+        restBetweenBlocks: "A feladat során néhány percenként lesz egy rövid szünet, amikor megmutatjuk számodra az átlagos pontosságod és reakcióidődet, amit felhasználhatsz a tejesítményed javításához.",
+        img: "<img src='../static/images/ASRT_hu.gif' width='30%' alt='Dalmata'/>"
+    },
+    instruction2: {
+        firstButton: "Az <strong>'S'</strong> billentyűt a bal kezed középsőujjával,",
+        secondButton: "az <strong>'F'</strong> billentyűt a bal kezed mutatóujjával,</strong>",
+        thirdButton: "a <strong>'J'</strong> billentyűt a jobb kezed mutatóujjával,",
+        fourthButton: "az <strong>'L'</strong> billentyűt pedig a jobb kezed középsőujjával </strong>kell lenyomni.",
+        ifClear: "Ha világosak az instrukciók, akkor kattints a <strong>Tovább</strong> gombra; amennyiben nem, a <strong>Vissza</strong> gomb megnyomásával visszamehetsz az előző oldalra." 
+    },
+    practice: {
+        startPractice: "Ha készenállsz, nyomd meg <strong>bármelyik</strong> gombot egy rövid gyakorlás megkezdéséhez!",
+        practiceSoon: "Most egy rövid gyakorlás következik."
+    },
+    task: {
+        realTask: "A feladat most kezdődik élesben.",
+        startTask: "Ha készen állsz, nyomd meg <strong>bármelyik</strong> gombot a kezdéshez!",
+        nextBlock: "Nyomd meg bármelyik gombot a következő blokk indításához!",
+        nextBlockSoon: "A következő blokk hamarosan indul.",
+        place: "Helyezd az ujjaidat az alábbi pozícióba a billentyűzeteden:"
+    },
+    end: {
+        endTask: "Vége a feladatnak.",
+        thankYou: "Köszönjük!",
+    },
+    feedback: {
+        endBlock: "Vége ennek a blokknak: ",
+        yourAccuracy: "Az átlagos pontosságod: ",
+        yourRt: "Az átlagos reakcióidőd: ",
+        moreAccurate: "Kérlek, próbálj meg pontosabb lenni!",
+        faster: "Kérlek, próbálj meg gyorsabb lenni!",
+        continue: "Szép munka!"
+    },
+    button: {
+        next: "Tovább",
+        previous: "Vissza"
+    },
+     fullscreen: {
+         fullscreenText: "A feladat teljes képernyőre ugrik, amint megnyomod a Tovább gombot.",
+         fullscreenButton: "Tovább"
+    },
+    parameters: {
+        subject: "Vizsgálati személy száma: ",
+        session: "Ülés száma: "
+    },
+    startWarning: {
+        startSubject: "Feladat kezdése <strong> ezzel a vizsgálati személy számmal: ",
+        startSession: "</strong> és <strong>ezzel az ülésszámmal: ",
+        startButton: "Feladat kezdése",
+        goBackButton: "Paraméterek változtatása"
+    },
+    warning: {
+        warning: "FIGYELEM!",
+        accuracy: "Az eddigi pontosságod a feladat egésze alatt:",
+        onlyValid: "Csak akkor tudjuk elfogadni a kitöltésed, ha a feladat végére 80% felett lesz a pontosságod!",
+        moreAccurate: "Kérlek, próbálj pontosabb lenni!",
+        pressButton: "Kérlek, nyomj meg egy gombot a folytatáshoz!"
+    }
+}
+
+const fr = {
+    welcomePage: {
+        welcome: "Bienvenue!",
+        clickNext: "Cliquez sur <strong>Suivant</strong> pour continuer."
+    },
+    instruction: {
+        fourCircles: "Dans cette tâche, vous verrez quatre cercles à l'écran. De gauche à droite, les touches <strong>'S'</strong>, <strong>'F'</strong>, <strong>'J'</strong> et <strong>'L'</strong> correspondent aux quatre cercles.",
+        dog: "De temps en temps, une <b>image d'un chien</b> apparaîtra dans l'un des cercles.",
+        yourTask: "Votre tâche consistera à 'attraper le chien' en appuyant sur la touche correspondant à la position du chien <strong>aussi vite et aussi précisément que possible</strong>.",
+        restBetweenBlocks: "À intervalle de quelques minutes, il y aura une courte pause, au cours de laquelle nous vous montrerons votre précision et votre temps de réponse moyens, que vous pourrez utiliser pour améliorer vos performances.",
+        img: "<img src='../static/images/ASRT_en.gif' width='30%' alt='Dalmata'/>"
+    },
+    instruction2: {
+        firstButton: "Vous devrez appuyer sur la touche <strong>'S'</strong> avec le majeur de votre main gauche,",
+        secondButton: "la touche <strong>'F'</strong> avec l'index de votre main gauche</strong>.",
+        thirdButton: "La touche <strong>'J'</strong> avec l'index de votre main droite,",
+        fourthButton: "la touche <strong>'L'</strong> avec le majeur de votre main droite</strong>",
+        ifClear: "Si les instructions sont claires, cliquez sur <strong>Suivant</strong> ; sinon, vous pouvez revenir en arrière et vérifier à nouveau les instructions en cliquant sur <strong>Précédent</strong>."
+    },
+    practice: {
+        startPractice: "Si vous êtes prêt, appuyez sur <strong>n'importe quelle touche</strong> du clavier pour commencer un entraînement!",
+        practiceSoon: "Maintenant un petit entraînement va démarrer."
+    },
+    task: {
+        realTask: "La vraie tâche va maintenant commencer.",
+        startTask: "Si vous êtes prêt, appuyez sur <strong>n'importe quelle touche</strong> pour commencer la tâche!",
+        nextBlock: "Appuyez sur n'importe quelle touche pour débuter le prochain bloc!",
+        nextBlockSoon: "Le prochain bloc va bientôt commencer.",
+        place: "Merci de placer vos doigt sur le clavier dans cette position:"
+    },
+    end: {
+        endTask: "Fin de l'expérience.",
+        thankYou: "Merci!"
+    },
+    feedback: {
+        endBlock: "Fin du bloc ",
+        yourAccuracy: "Votre précision: ",
+        yourRt: "Votre temps de réponse moyen: ",
+        moreAccurate: "Essayez d'être plus précis!",
+        faster: "Essayez d'aller plus vite!",
+        continue: "Bien joué!"
+    },
+    button: {
+        next: "Suivant",
+        previous: "Précédent"
+    },
+    fullscreen: {
+        fullscreenText: "L'expérience passera en mode plein écran lorsque vous appuyerez sur le bouton ci-dessous",
+        fullscreenButton: "Continuer"
+    },
+    parameters: {
+        subject: "Numéro du sujet : ",
+        session: "Numéro de session : "
+    },
+    startWarning: {
+        startSubject: "Vous êtes sur le point de commencer la tâche avec <strong>Numéro de sujet ",
+        startSession: "</strong> et <strong>Numéro de session ",
+        startButton: "Commencez la tâche !",
+        goBackButton: "Modifier les paramètres"
+    },
+    warning: {
+        warning: "ATTENTION!",
+        accuracy: "Votre précision tout au long de la tâche :",
+        onlyValid: "Nous ne pouvons accepter votre soumission que si votre précision est supérieure à 80 % à la fin de la tâche.",
+        moreAccurate: "Essayez d'être plus précis !",
+        pressButton: "Veuillez appuyer sur un bouton pour continuer !"
+    }
+}
+
+const pt = {
+    welcomePage: {
+        welcome: "Bem-vindo!",
+        clickNext: "Clique em <strong>Próximo</strong> para continuar."
+    },
+    instruction: {
+        fourCircles: "Nessa tarefa, você verá quatro círculos na tela. Da esquerda para a direita, as teclas <strong>'S'</strong>, <strong>'F'</strong>, <strong>'J'</strong> e <strong>'L'</strong> correspondem aos quatro círculos.",
+        dog: "Às vezes, uma <b>imagem de um cachorro</b> vai aparecer em um dos círculos.",
+        yourTask: "Sua tarefa será 'capturar o cachorro' apertando a tecla correspondente à posição do cachorro, e sendo <strong>o mais rápido e preciso que você puder</strong>.",
+        restBetweenBlocks: "A cada poucos minutos, haverá uma pequena pausa, quando vamos te mostrar sua média de precisão e seu tempo de resposta, que você pode utilisar para melhorar sua performance.",
+        img: "<img src='../static/images/ASRT_en.gif' width='30%' alt='Dalmata'/>"
+    },
+    instruction2: {
+        firstButton: "Você vai precisar apertar a tecla <strong>'S'</strong> com o dedo do meio da sua mão esquerda,",
+        secondButton: "a tecla <strong>'F'</strong> com o dedo indicador da sua mão esquerda</strong>",
+        thirdButton: "a tecla <strong>'J'</strong> com o dedo indicador da sua mão direita,",
+        fourthButton: "a tecla <strong>'L'</strong> com o dedo do meio da sua mão direita</strong>.",
+        ifClear: "Se as instruções estão claras, clique em <strong>Próximo</strong>; senão, você pode voltar e checar de novo as instruções clicando em <strong>Anterior</strong>."
+    },
+    practice: {
+        startPractice: "Se você estiver pronto, aperte <strong>qualquer</strong> tecla para começar um treino!",
+        practiceSoon: "Agora você terá um pequeno treino."
+    },
+    task: {
+        realTask: "A verdadeira tarefa começa agora.",
+        startTask: "Se você estiver pronto, aperte <strong>qualquer</strong> tecla para começar a tarefa!",
+        nextBlock: "Aperte qualquer tecla para iniciar o próximo bloco!",
+        nextBlockSoon: "O próximo bloco começará logo.",
+        place: "Por favor posicione seus dedos nesta posição no teclado:"
+    },
+    end: {
+        endTask: "Fim do experimento.",
+        thankYou: "Muito obrigado!"
+    },
+    feedback: {
+        endBlock: "Fim do bloco ",
+        yourAccuracy: "Sua precisão: ",
+        yourRt: "Seu tempo de resposta médio: ",
+        moreAccurate: "Tente ser mais preciso!",
+        faster: "Tente ser mais rápido!",
+        continue: "Bom trabalho!"
+    },
+    button: {
+        next: "Próximo",
+        previous: "Anterior"
+    },
+    fullscreen: {
+        fullscreenText: "O experimento irá passar para o modo tela cheia quando você clicar no botão abaixo.",
+        fullscreenButton: "Continue"
+    },
+    parameters: {
+        subject: "Assunto número: ",
+        session: "Número da sessão: "
+    },
+    startWarning: {
+        startSubject: "Você está prestes a iniciar a tarefa com<strong> Número do assuntot ",
+        startSession: "</strong> e <strong>Sessão número ",
+        startButton: "Comece a tarefa!",
+        goBackButton: "Alterar os parâmetros"
+    },
+    warning: {
+        warning: "ATENÇÃO!",
+        accuracy: "A sua precisão ao longo de toda a tarefa:",
+        onlyValid: "Só podemos aceitar a sua submissão se tiver mais de 80% de exactidão no final do trabalho.",
+        moreAccurate: "Tente ser mais preciso!",
+        pressButton: "Por favor, prima um botão para continuar!"
+    }
+}
+
+const es = {
+    welcomePage: {
+        welcome: "Bienvenid@!",
+        clickNext: "Haz clic en <strong>Siguiente</strong> para continuar."
+    },
+    instruction: {
+        fourCircles: "En esta tarea verás aparecer cuatro círculos en pantalla. De izquierda a derecha, las teclas <strong>'S'</strong>, <strong>'F'</strong>, <strong>'J'</strong> and <strong>'L'</strong> corresponden a los cuatro círculos.",
+        dog: "Cada tanto, la <b>imagen de un perro</b> irá apareciendo en uno de los cuatro círculos.",
+        yourTask: "Tu objetivo es 'atrapar al perro' haciendo clic en una de las cuatro teclas correspondientes a la localización del perro. <strong>Intenta ser lo más rápid@ y precis@ que puedas</strong>.",
+        restBetweenBlocks: "Cada pocos minutos, habrá una pausa en la que te mostraremos tus promedios de precisión y de tiempo de reacción. Podrás usar estas informaciones para mejorar tu desepeño.",
+        img: "<img src='../static/images/ASRT_en.gif' width='30%' alt='Dalmata'/>"
+    },
+    instruction2: {
+        firstButton: "Presiona  la tecla <strong>'S'</strong>con el dedo medio de tu mano izquierda,",
+        secondButton: "La tecla <strong>'F'</strong> con el dedo índice de tu mano izquierda</strong>",
+        thirdButton: "La tecla <strong>'J'</strong> con el dedo índice de tu mano derecha ,",
+        fourthButton: "La tecla <strong>'L'</strong> con el dedo medio de tu mano derecha</strong>.",
+        ifClear: "Si has entendido las instrucciones, haz clic en <strong>Siguiente</strong>; de lo contrario, puedes volver a ver las instrucciones haciendo clic en <strong>Atrás</strong>."
+    },
+    practice: {
+        startPractice: "Si estás list@, presiona <strong>cualquier</strong> tecla para empezar el entrenamiento!",
+        practiceSoon: "Un entrenamiento corto empezará ahora."
+    },
+    task: {
+        realTask: "Fin del entrenamiento. La verdadera tarea empieza ahora.",
+        startTask: "Si estás list@, presiona <strong>cualquier</strong> tecla para empezar la tarea!",
+        nextBlock: "Presiona cualquier tecla para empezar el siguiente bloque!",
+        nextBlockSoon: "El próximo bloque empezará dentro de poco",
+        place: "Por favor coloca tus dedos en el teclado de la siguiente manera."
+    },
+    end: {
+        endTask: "Fin del experimento.",
+        thankYou: "Gracias!"
+    },
+    feedback: {
+        endBlock: "Fin del bloque ",
+        yourAccuracy: "Tu precisión fue: ",
+        yourRt: "Tu tiempo de reacción promedio fue: ",
+        moreAccurate: "Intenta ser más precis@!",
+        faster: "Intenta ser más rápid@!",
+        continue: "Bien hecho!"
+    },
+    button: {
+        next: "Siguiente",
+        previous: "Atrás"
+    },
+    fullscreen: {
+        fullscreenText: "El experimento pasará a pantalla completa al cliquear en Continuar.",
+        fullscreenButton: "Continuar"
+    },
+    parameters: {
+        subject: "Número de asunto: ",
+        session: "Número de sesión: "
+    },
+    startWarning: {
+        startSubject: "Está a punto de iniciar la tarea con <strong>Número de Asunto ",
+        startSession: "</strong> y <strong>Número de Sesión ",
+        startButton: "¡Inicia la tarea!",
+        goBackButton: "Cambiar los parámetros"
+    },
+    warning: {
+        warning: "¡ATENCIÓN!",
+        accuracy: "Su precisión a lo largo de toda la tarea:",
+        onlyValid: "Sólo podemos aceptar su envío si su precisión es superior al 80% al final de la tarea.",
+        moreAccurate: "Por favor, intente ser más preciso.",
+        pressButton: "Por favor, pulse un botón para continuar."
+    }
+}

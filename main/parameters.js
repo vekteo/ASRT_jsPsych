d036bfef0196d9f03bb7872304393f517849761d
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2022-10-06 14:55:22 +0200

    add new parameters

---
+++ main/parameters.js
@@ -0,0 +1,14 @@
+/*************** PARAMETERS OF THE TASK ***************/
+
+const language = hu; //available languages: english (en), hungarian (hu), french (fr), portoguise (pt)
+const numberOfPracticeBlocks = 1;
+const numberOfBlocks = 20;
+const numberOfSequenceRepetitions = 10;
+const patternTrialImage = "url(../static/images/dalmata.jpg)";
+const randomTrialImage = "url(../static/images/dalmata.jpg)";
+const rsi = 120;
+const initialDelay = 1000;
+const overallWarning = false;
+const isOffline = true;
+const responseKeys = [['s', 'f', 'j', 'l']];
+const isFirstFiveRandom = false;

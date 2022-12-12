a7e21ba046348c558ac5f7c2dd7cf535c9e6e896
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2022-12-12 14:19:37 +0100

    reset parameters

--- main/parameters.js
+++ main/parameters.js
@@ -1,8 +1,8 @@
 /*************** PARAMETERS OF THE TASK ***************/
 
-const language = fr; //available languages: english (en), hungarian (hu), french (fr), portuguese (pt)
-const numberOfPracticeBlocks = 0;
-const numberOfBlocks = 15;
+const language = en; //available languages: english (en), hungarian (hu), french (fr), portuguese (pt)
+const numberOfPracticeBlocks = 1;
+const numberOfBlocks = 20;
 const numberOfSequenceRepetitions = 10;
 const patternTrialImage = "url(../static/images/dalmata.jpg)";
 const randomTrialImage = "url(../static/images/dalmata.jpg)";
